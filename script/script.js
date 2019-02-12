7db953126e54d3f7b13e3d49a8228ec26d2eba29
kishor <kishorrathva8298@gmail.com>
2019-02-13 00:10:35 +0530

    Changed forEach to regular for becouse forEach dont allow break !!

--- script/script.js
+++ script/script.js
@@ -1,5 +1,3 @@
-
-
 let submitButton = document.querySelector('button[type="submit"]');
 // let sumt = document.getElementsByTagName('button');
 
@@ -8,38 +6,35 @@ let submitButton = document.querySelector('button[type="submit"]');
 // Link to fetch data
 // https://api.myjson.com/bins/14x586
 
+submitButton.addEventListener("click", login);
 
-submitButton.addEventListener('click',login);
+function login() {
+  let usrname = document.getElementById("userName").value;
+  let pswd = document.getElementById("pwd").value;
+  fetch("https://api.myjson.com/bins/y337a")
+    .then(function(response) {
+      return response;
+    })
+    .then(function(e) {
+      return e.json();
+    })
+    .then(function(data) {
+      console.log(data);
 
-function login(){
-	let usrname=document.getElementById("userName").value;
-	fetch('https://api.myjson.com/bins/y337a')
-	.then(function(response){
-		return response;
-	})
-	.then(function(e){
-		return e.json();
-	})
-	.then(function(data){
-		console.log(data);
-		data.forEach(function(users){
-			if(usrname == users.usrName){
-				console.log(usrname);
-				console.log(users.admin);
-				if(users.admin){
-					alert("Welcom Admin");
-				
-				}else{
-					alert("Welcom");
-
-				}
-			}
-		})
-	})
-	.catch(function(error){
-			console.log(error);
-	})
-
-} 
-
-
+      for (let i in data) {
+        if (usrname == data[i].usrName && pswd == data[i].passWord) {
+          console.log(usrname);
+          console.log(data[i].admin);
+          if (data[i].admin) {
+            alert("Welcom kishor");
+            break;
+          } else {
+            alert("Welcom");
+            break;
+          }
+        } else {
+          alert("Credential invalid");
+        }
+      }
+    });
+}
