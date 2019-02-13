73928f2fdbbf3afb2c517a3f9583674b60fe5e06
Kishor Rathva <kishor.rathva@rapidops.com>
2019-02-13 10:58:16 +0530

    Login Module added

--- script/script.js
+++ script/script.js
@@ -11,6 +11,7 @@ submitButton.addEventListener("click", login);
 function login() {
   let usrname = document.getElementById("userName").value;
   let pswd = document.getElementById("pwd").value;
+  let flag=0;
   fetch("https://api.myjson.com/bins/y337a")
     .then(function(response) {
       return response;
@@ -20,21 +21,24 @@ function login() {
     })
     .then(function(data) {
       console.log(data);
+		for (let i in data) {
+        	if (usrname == data[i].usrName && pswd == data[i].passWord) {
+          		flag=1;
+         		if (data[i].admin) {
+            		alert("Welcome Admin");
+            		break;
+          		} else {
+            		alert("Welcome");
+            		break;
+          		}
+        	} else {
+        		flag=0;
+          		continue;
+        	}
+      	}
+      	if(!flag){
+      		alert("Invalid Credentials..");
+      	}
 
-      for (let i in data) {
-        if (usrname == data[i].usrName && pswd == data[i].passWord) {
-          console.log(usrname);
-          console.log(data[i].admin);
-          if (data[i].admin) {
-            alert("Welcom kishor");
-            break;
-          } else {
-            alert("Welcom");
-            break;
-          }
-        } else {
-          alert("Credential invalid");
-        }
-      }
     });
 }
