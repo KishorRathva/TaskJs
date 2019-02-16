2a2535ab6f829963d47d305e2295fecb92d4e7d4
kishor <kishorrathva8298@gmail.com>
2019-02-17 03:14:49 +0530

    Almost nothing changed (use for fetch API )

--- script/script.js
+++ script/script.js
@@ -1,17 +1,10 @@
 let submitButton = document.querySelector('button[type="submit"]');
-// let sumt = document.getElementsByTagName('button');
-
-// sumt.addEventListener("click",login,false);
-
-// Link to fetch data
-// https://api.myjson.com/bins/14x586
-
 submitButton.addEventListener("click", login);
 
 function login() {
   let usrname = document.getElementById("userName").value;
   let pswd = document.getElementById("pwd").value;
-  let flag=0;
+  let flag = 0;
   fetch("https://api.myjson.com/bins/y337a")
     .then(function(response) {
       return response;
@@ -21,26 +14,26 @@ function login() {
     })
     .then(function(data) {
       console.log(data);
-		for (let i in data) {
-        	if (usrname == data[i].usrName && pswd == data[i].passWord) {
-          		flag=1;
-         		if (data[i].admin) {
-            		window.location = "home.html?admin="+data[i].admin;
-            		break;
-          		} else {
-            		window.location = "home.html?admin="+data[i].admin;
-            		break;
-          		}
-        	} else {
-        		flag=0;
-          		continue;
-        	}
-      	}
-      	if(!flag){
-      		alert("Invalid Credentials..");
-      	}
-
-    }).catch(function(result){
-    	console.log(result);
+      for (let i in data) {
+        if (usrname == data[i].usrName && pswd == data[i].passWord) {
+          flag = 1;
+          if (data[i].admin) {
+            window.location = "home.html?admin=" + data[i].admin;
+            break;
+          } else {
+            window.location = "home.html?admin=" + data[i].admin;
+            break;
+          }
+        } else {
+          flag = 0;
+          continue;
+        }
+      }
+      if (!flag) {
+        alert("Invalid Credentials..");
+      }
+    })
+    .catch(function(result) {
+      console.log(result);
     });
 }
